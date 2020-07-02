daf515d50287522682add913ed11b30ef6d7635d
Bipin Advani <56110542+bipin1909@users.noreply.github.com>
2020-07-02 21:45:52 +0530

    Add files via upload

---
+++ Face detection and bluring in webcam using cv2.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+blur = False
+framed = False
+
+while True:
+    ret,frame = cap.read()
+    if(ret):
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+        f = face.detectMultiScale(gray,scaleFactor = 1.1,minNeighbors = 7)
+
+        for x,y,w,h in f:
+            if blur:
+                # create a sub face from detected faces:
+                sub_face = frame[y:y+h, x:x+w]
+
+                # apply a gaussian blur on this new recangle image
+                sub_face = cv2.GaussianBlur(sub_face,(23, 23), 30)
+
+                # merge this blurry rectangle to our final image
+                frame[y:y+sub_face.shape[0], x:x+sub_face.shape[1]] = sub_face
+
+
+
+                
+            if framed:
+                cv2.rectangle(frame,(x,y),(x+h,y+w),(0,255,0),2)
+        cv2.imshow('Face Recognized',frame)
+    ch = 0xFF &cv2.waitKey(1)
+    
+    if ch == ord("b"):
+        blur = not blur
+
+    if ch == ord("f"):
+        framed = not framed
+
+    if ch ==ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
